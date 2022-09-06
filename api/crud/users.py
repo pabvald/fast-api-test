c626b5ab0f5360ac70f26f4abd9efffc3947aec8
pabvald <valduncielpablo@gmail.com>
2022-09-06 18:31:06 +0200

    add small doc to CRUD operations

--- api/crud/users.py
+++ api/crud/users.py
@@ -11,18 +11,22 @@ from models.user import UserCreate, UserUpdate
 
 
 def get_user(db: Session, user_id: UUID):
+    """Gets a User by id"""
     return db.query(UserModel).filter(UserModel.id == user_id).first()
 
 
 def get_user_by_name(db: Session, name: str):
+    """Gets a User by name"""
     return db.query(UserModel).filter(UserModel.name == name).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
+    """Gets all users"""
     return db.query(UserModel).offset(skip).limit(limit).all()
 
 
 def get_users_by_tv_show(db: Session, tv_show: TVShow, skip: int = 0, limit: int = 100):
+    """Gets all users that watch a certain tv show"""
     return (
         db.query(UserModel)
         .filter(UserModel.favorite_tv_show == tv_show)
@@ -33,6 +37,7 @@ def get_users_by_tv_show(db: Session, tv_show: TVShow, skip: int = 0, limit: int
 
 
 def create_user(db: Session, user: UserCreate):
+    """Creates a new User"""
     # create user
     db_user = UserModel(name=user.name, favorite_tv_show=user.favorite_tv_show)
     # save to db
@@ -43,19 +48,22 @@ def create_user(db: Session, user: UserCreate):
 
 
 def delete_user(db: Session, db_user: UserModel):
+    """Deletes a User"""
     if db_user:
         db.delete(db_user)
         db.commit()
     return db_user
 
 
-def update_user(db: Session, db_user: UserModel, update_user: UserUpdate):
+def modify_user(db: Session, db_user: UserModel, update_user: UserUpdate):
+    """Modifies a User"""
+    if db_user.id != update_user.id:
+        raise Exception("the UserUpdate doesn't correspond to the queried User")
 
-    if db_user.id == update_user.id:
-        if update_user.name:
-            db_user.name = update_user.name
-        if update_user.favorite_tv_show:
-            db_user.favorite_tv_show = update_user.favorite_tv_show
-        db.commit()
+    if update_user.name:
+        db_user.name = update_user.name
+    if update_user.favorite_tv_show:
+        db_user.favorite_tv_show = update_user.favorite_tv_show
+    db.commit()
 
     return db_user
